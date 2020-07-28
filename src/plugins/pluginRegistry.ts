2a99d34cb0a9684f55996cf24f68fadc126665de
Billy Charlton <billy@okbecause.com>
2020-07-28 17:46:24 +0200

    feat(video): First version of Video Player plugin!
    Only works with MP4 files

--- src/plugins/pluginRegistry.ts
+++ src/plugins/pluginRegistry.ts
@@ -1,11 +1,19 @@
-// import the plugins in the order you want them to appear on project pages
+// import plugins in the order you want them to appear on project pages
 import AgentAnimation from '@/plugins/agent-animation/AgentAnimation.vue'
 import SankeyDiagram from '@/plugins/sankey/SankeyDiagram.vue'
 import AggregateOd from '@/plugins/aggregate-od/AggregateOd.vue'
 import TransitSupply from '@/plugins/transit-supply/TransitSupply.vue'
+import VideoPlayer from '@/plugins/video-player/VideoPlayer.vue'
 import ImageView from '@/plugins/image/ImageView.vue'
 
 // EVERY plugin must be registered here:
-const plugins = { AgentAnimation, SankeyDiagram, AggregateOd, TransitSupply, ImageView }
+const plugins = {
+  AgentAnimation,
+  SankeyDiagram,
+  AggregateOd,
+  TransitSupply,
+  VideoPlayer,
+  ImageView,
+}
 
 export default plugins
