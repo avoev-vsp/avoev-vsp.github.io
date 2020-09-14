52b537b3732be87e818ce38863b71df58094865c
Billy Charlton <billy@okbecause.com>
2020-09-14 10:45:32 +0200

    add deck.gl / react

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,20 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['plugin:vue/essential', '@vue/prettier', '@vue/typescript'],
+  extends: ['plugin:vue/essential', '@vue/prettier', '@vue/typescript', 'plugin:react/recommended'],
   rules: {
     'no-console': 'off', // process.env.NODE_ENV === "production" ? "error" : "off",
-    'no-debugger': 'off', // process.env.NODE_ENV === "production" ? "error" : "off"
+    'no-debugger': 'off', // process.env.NODE_ENV === "production" ? "error" : "off",
+    'react-hooks/rules-of-hooks': 'error',
+    // 'react-hooks/exhaustive-deps': 'warn',
+    'react/prop-types': 'off',
   },
   parserOptions: {
     parser: '@typescript-eslint/parser',
+    ecmaVersion: 2018,
+    sourceType: 'module',
   },
+  plugins: ['@typescript-eslint', 'react-hooks'],
   overrides: [
     {
       files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
@@ -19,4 +25,10 @@ module.exports = {
       },
     },
   ],
+  settings: {
+    react: {
+      pragma: 'React',
+      version: 'detect',
+    },
+  },
 }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import App from '@/App.vue'
 import router from '@/router'
 import store from '@/store'
 import Buefy from 'buefy'
+import { VuePlugin } from 'vuera'
+
+Vue.use(VuePlugin)
 
 Vue.use(Buefy, {
   defaultIconPack: 'mdi',
